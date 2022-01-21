dcf43e6347e836cac7eff266e198e9063b2b9527
Onur AĞA <onuraga@windowslive.com>
2022-01-21 08:42:06 +0300

    Add project files.

---
+++ MvcCrud/Controllers/CategoriesController.cs
@@ -0,0 +1,46 @@
+﻿using MvcCrud.Models;
+using MvcCrud.Models.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcCrud.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        // GET: Categories
+        NorthwindEntities db = new NorthwindEntities();
+        CategoriesModel model = new CategoriesModel();
+        public ActionResult Liste(string name)
+        {
+            if (name == null)
+            {
+                name = "";
+            }
+            //Catagorı tablaosunda sarta bakıcak, ıcersınde parametre olarak name varmı dıye bakıyor
+            model.cList = db.Categories.Where(x => x.CategoryName.Contains(name)).ToList();
+            return View(model);
+        }
+        public ActionResult Detay(int id)
+        {
+            model.categories = db.Categories.Find(id);
+            return View(model);
+        }
+        public ActionResult Guncelle(int id)
+        {
+            model.categories = db.Categories.Find(id);
+            return View(model);
+
+        }
+        public ActionResult Sil(int id)
+        {
+            var urun = db.Categories.Find(id);
+            db.Categories.Remove(urun);
+
+
+            return View();
+        }
+    }
+}
---
+++ MvcCrud/Models/Dto/CategoriesModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcCrud.Models.Dto
+{
+    public class CategoriesModel
+    {
+        public List<Categories> cList { get; set; }
+        public Categories categories { get; set; }
+    }
+}
